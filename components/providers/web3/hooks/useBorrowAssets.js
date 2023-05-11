7341e8c81945c638b79f686234dc0b399d2dc767
Cooller458 <omrelibol4@gmail.com>
2023-05-11 12:13:13 +0300

    add : create borrow asset list hooks

---
+++ components/providers/web3/hooks/useBorrowAssets.js
@@ -0,0 +1,42 @@
+import useSWR from "swr"
+
+import { normalizeToken } from "../../../utils/normalize"
+
+const NETWORKS = {
+    1: "Ethereum Mainnet",
+    3: "Ropsten Test Network",
+    4: "Rinkeby Test Network",
+    5: "Goerli Test Network",
+    42: "Kovan Test Network",
+    56: "Binance Smart Chain",
+    97: "Binance Smart Chain Testnet",
+    1337: "Localhost 8545",
+};
+
+export const handler = (web3, contract) => () => {
+    const { data, error, mutate, ...rest } = useSWR(
+        () => (web3 ? "web3/supply_assets" : null),
+        async () => {
+            const borrowAssets = [];
+            const tokens = await contract.method.getTokensForBorrowingArray().call();
+
+            for (let i = 0; i < tokens.length; i++) {
+                const currentToken = tokens[i];
+                const newToken = await normalizeToken(web3, contract, currentToken)
+                
+                borrowAssets.push(newToken);
+            }
+            return borrowAssets;
+        }
+    );
+
+    const targetNetwork = NETWORKS[97];
+
+    return {
+        data,
+        error,
+        ...rest,
+        target: targetNetwork,
+        isSupported: data === targetNetwork,
+    };
+}
