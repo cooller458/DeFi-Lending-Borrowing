c9314ac8ecd169690c109bcf09f2d311fafc6f0f
Cooller458 <omrelibol4@gmail.com>
2023-05-15 14:22:17 +0300

    create components ui navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import { Web3Provider } from "../components/providers";
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <Web3Provider>
+      <Component {...pageProps} />
+    </Web3Provider>
+  )
 }
+
+export default MyApp
