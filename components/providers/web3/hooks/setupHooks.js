9d4e97865e59e4578648982f484547d4027c7fe6
Cooller458 <omrelibol4@gmail.com>
2023-05-15 13:53:48 +0300

    add : setup hooks

---
+++ components/providers/web3/hooks/setupHooks.js
@@ -0,0 +1,17 @@
+import { handler as createAccountHook } from "./useAccount"
+import { handler as createNetworkHook } from "./useNetwork"
+import { handler as createSupplyAssetsHook } from "./useSupplyAssets"
+import { handler as createBorrowAssetsHook } from "./useBorrowAssets"
+import { handler as createYourSuppliesHook } from "./useYourSupplies"
+import { handler as createYourBorrowsHook } from "./useYourBorrows"
+
+export const setupHooks = ({ web3, provider, contract }) => {
+    return {
+        useAccount: createAccountHook(web3, provider),
+        useNetwork: createNetworkHook(web3),
+        useSupplyAssets: createSupplyAssetsHook(web3, contract),
+        useBorrowAssets: createBorrowAssetsHook(web3, contract),
+        useYourSupplies: createYourSuppliesHook(web3, contract),
+        useYourBorrows: createYourBorrowsHook(web3, contract)
+    }
+}
